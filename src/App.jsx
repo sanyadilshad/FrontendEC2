25844d3187f4e1776eb15c505e88eb4de2ec06bd
= <=>
2025-02-17 14:54:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>This is new EC2 project</h1>
+      <h2>Welcome here</h2>
+    </div>
+  )
+}
+
+export default App
