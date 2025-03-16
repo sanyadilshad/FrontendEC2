722d5e8c35fae0d6eae961a9da591535dea53155
= <=>
2025-03-16 08:50:28 +0530

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1>This is new EC2 project</h1>
-      <h2>Welcome here</h2>
+      <h1>Welcome to My Deployed Frontend</h1>
+      <p>
+        This is a simple frontend project deployed on AWS EC2 by Sanya Dilshad.
+      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
